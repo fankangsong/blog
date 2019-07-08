6d2a30b7487a5ee891ab824267f8882b72184c54
fankangsong <fankangsong@gmail.com>
2019-07-08 20:59:33 +0800

    update

--- src/components/footer.js
+++ src/components/footer.js
@@ -5,7 +5,12 @@ const Footer = () => {
   const fullyear = new Date().getFullYear()
   return (
     <footer className="site-footer code">
-      <img src={Icon} className="icon" style={{ width: 16, height: 16 }} />
+      <img
+        src={Icon}
+        alt="imfer.me"
+        className="icon"
+        style={{ width: 16, height: 16 }}
+      />
       <span className="copyright">&copy; {fullyear} </span>
       <span>imfer.me</span>
     </footer>
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,13 +1,14 @@
 import React from 'react'
-import { Link } from 'gatsby'
 
 import Layout from '../components/layout'
 import HomeImg from '../components/home'
 
 function IndexPage(props) {
-  return <Layout props={props}>
-    <HomeImg />
-  </Layout>
+  return (
+    <Layout props={props}>
+      <HomeImg />
+    </Layout>
+  )
 }
 
 export default IndexPage
