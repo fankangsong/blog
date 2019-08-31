e270527666cf60b1471e35056171433aba69a777
fankangsong <fankangsong@gmail.com>
2019-08-31 14:50:06 +0800

    ...

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,8 +4,9 @@ import Layout from '../components/layout'
 function IndexPage(props) {
   return (
     <Layout props={props} title="首页">
-      <div className="content">
-        <p>这个家伙太懒了，什么都没留下。</p>
+      <div className="content home">
+        <p>&nbsp;</p>
+        <p className="slogan">这个家伙太懒了，什么都没留下。</p>
       </div>
     </Layout>
   )
