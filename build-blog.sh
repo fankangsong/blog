13977235fdf272e68aa9d2592ef0da62a44e19d2
root <root@localhost.localdomain>
2021-06-10 14:31:33 +0800

    update script

--- build-blog.sh
+++ build-blog.sh
@@ -10,4 +10,5 @@ npm run build
 
 cp -rf public ../html/imcolin.fan/
 
-curl https://service-97nccwao-1251499259.ap-hongkong.apigateway.myqcloud.com/release/telebot?msg=blog-is-published
+MSG="blog发布啦！"
+curl https://service-97nccwao-1251499259.ap-hongkong.apigateway.myqcloud.com/release/telebot?msg=$MSG
