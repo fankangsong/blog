6ffad0ff002c8da5331b1ea86158ffe693128820
fankangsong <fankangsong@gmail.com>
2021-08-28 23:10:23 +0800

    add script

---
+++ before-publish-blog.sh
@@ -0,0 +1,5 @@
+#!/bin/sh -i
+
+export PATH=/root/.nvm/versions/node/v10.24.1/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games:/snap/bin
+
+rm -rf /root/html/imcolin.fan/public
