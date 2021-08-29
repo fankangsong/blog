79507c340522fafcc75a8f8b03ed2fcccade228f
fankangsong <fankangsong@gmail.com>
2021-08-29 19:11:05 +0800

    update1

--- build-blog.sh
+++ build-blog.sh
@@ -2,13 +2,13 @@
 
 export PATH=/root/.nvm/versions/node/v10.24.1/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games:/snap/bin
 
-cd blog
+# cd blog
 
-git pull
+# git pull
 
-npm run build
+# npm run build
 
-cp -rf public ../html/imcolin.fan/
+# cp -rf public ../html/imcolin.fan/
 
-MSG="blog发布啦！"
+MSG="blog发布啦！https://imcolin.fan"
 curl https://service-97nccwao-1251499259.ap-hongkong.apigateway.myqcloud.com/release/telebot?msg=$MSG
