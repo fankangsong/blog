9b09de70e564db3d5a8ad13d27fe308db08ac693
fankangsong <fankangsong@gmail.com>
2024-01-13 15:59:45 +0800

    update1

--- build-blog.sh
+++ build-blog.sh
@@ -1,5 +1,7 @@
 #!/bin/sh -i
 
+PATH=/root/.nvm/versions/node/v16.20.2/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games:/snap/bin
+
 cd /root/blog
 
 git pull
