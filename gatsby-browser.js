10b5ba2588605bcd1028f70612b7820b418cc2e2
fanks <fanks@yanxintec.com>
2024-04-03 17:05:44 +0800

    update

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -6,15 +6,3 @@
 
 // You can delete this file if you're not using it
 
-const DEV_HOST = ['localhost', 'myblog.com', 'mublog.dev']
-
-export const onClientEntry = () => {
-  const { hostname } = window.location;
-  if (hostname === 'localhost') return;
-
-  var _hmt = _hmt || [];
-  var hm = document.createElement('script');
-  hm.src = "https://hm.baidu.com/hm.js?c7ed426d8df2bf6697973aef72869051";
-  var s = document.getElementsByTagName('script')[0];
-  s.parentNode.insertBefore(hm, s);
-}
