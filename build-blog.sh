532fde5ee3abd050c1c51f184b4d745bc626fe62
fankangsong <fankangsong@gmail.com>
2024-08-23 02:37:49 +0000

    update

--- build-blog.sh
+++ build-blog.sh
@@ -2,11 +2,11 @@
 
 PATH=/sbin:/bin:/usr/sbin:/usr/bin:/usr/syno/sbin:/usr/syno/bin:/usr/local/sbin:/usr/local/bin:/var/services/homes/fankangsong/.local/bin
 
-cd /var/services/homes/fankangsong/code/blog
+cd /var/services/homes/fankangsong/code/workspace/blog
 
 git pull
 npm run build
 # scp -r public blog:/root/html/imcolin.fan/
 
-rsync -avz -P public blog:/root/html/imcolin.fan/
+rsync -avz --chmod=777 -P public blog:/root/html/imcolin.fan/
 sh /var/services/homes/fankangsong/code/notify.sh "blog已部署完成✅"
