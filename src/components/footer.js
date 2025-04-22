7957808b4422a8b6f6b94310956455f8f5f514de
fankangsong <fankangsong@gmail.com>
2025-04-22 02:54:19 +0000

    update 1

--- src/components/footer.js
+++ src/components/footer.js
@@ -6,17 +6,16 @@ const Footer = () => {
   const fullyear = new Date().getFullYear()
   return (
     <>
-    <footer className="site-footer code">
-      <img
-        src={Icon}
-        alt="imcolin.fan"
-        className="icon"
-        style={{ width: 16, height: 16 }}
-      />
-      <span className="copyright">&copy; {fullyear} </span>
-      <span>imcolin.fan</span>
-      <a href="https://www.travellings.cn/go-by-clouds.html" target="_blank" className="travellings">🚇开往</a>
-    </footer>
+      <footer className="site-footer code">
+        <img
+          src={Icon}
+          alt="imcolin.fan"
+          className="icon"
+          style={{ width: 16, height: 16 }}
+        />
+        <span className="copyright">&copy; {fullyear} </span>
+        <span>imcolin.fan</span>
+      </footer>
     </>
   )
 }
