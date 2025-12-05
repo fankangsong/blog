8795c4d3fd78b308a3b63c527d01ba2d6cc799d4
fankangsong <fankangsong@gmail.com>
2025-12-06 01:21:13 +0800

    1

--- sync.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh -i
-
-curl -X POST -d "token=Colin123.com&msg=开始同步" https://imcolin.fan/notify
-
-rsync -avz --chmod=777 -P post blog:/root/blog/
-
-curl -X POST -d "token=Colin123.com&msg=开始完成" https://imcolin.fan/notify
-
-ssh root@blog "sh /root/blog/run.sh"
